cc0dd17658045fe82fa9b1e7fd07e432a1db4f97
Mats Karlsson <mats.o.karlsson@gmail.com>
2024-06-02 11:57:31 +0200

    Update Page_Admin.h
    to English

--- Page_Admin.h
+++ Page_Admin.h
@@ -1,5 +1,3 @@
-
-
 //
 //  HTML PAGE
 //
@@ -7,16 +5,16 @@
 const char PAGE_AdminMainPage[] PROGMEM = R"=====(
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<strong>Управление устройством</strong>
+<strong>Device management</strong>
 <hr>
-<a href="config.html" style="width:250px" class="btn btn--m btn--blue" >Настройки сети</a><br>
-<a href="general.html" style="width:250px" class="btn btn--m btn--blue" >Настройки погоды</a><br>
-<a href="ntp.html"   style="width:250px"  class="btn btn--m btn--blue" >Настройки NTP</a><br>
-<a href="info.html"   style="width:250px"  class="btn btn--m btn--blue" >Сетевая информация</a><br>
+<a href="config.html" style="width:250px" class="btn btn--m btn--blue" >Network settings</a><br>
+<a href="general.html" style="width:250px" class="btn btn--m btn--blue" >Weather settings</a><br>
+<a href="ntp.html"   style="width:250px"  class="btn btn--m btn--blue" >NTP settings</a><br>
+<a href="info.html"   style="width:250px"  class="btn btn--m btn--blue" >Network information</a><br>
 
 <br>
-<td align="center">Рекомендую настраивать по порядку!</td><br><br>
-<td align="center">Разработчик: Марсель Ахкамов</td><br>
+<td align="center">I recommend setting up in order!</td><br><br>
+<td align="center">Developer: Marcel Akhkamov</td><br>
 <td align="center">  danubanan@gmail.com</td><br>
 <td align="center">  musa.pro@yandex.ru</td><br>
 <a href="http://vk.com/danubanan" align="center" target="_blank">vk.com/danubanan</a><br>
@@ -36,5 +34,3 @@ function load(e,t,n){if("js"==t){var a=document.createElement("script");a.src=e,
 </script>
 
 )=====";
-
-
